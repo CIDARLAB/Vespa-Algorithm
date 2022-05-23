b6df2b631898741486667124cf76f51666a19789
zyrrron <1043251725@qq.com>
2022-05-23 18:35:40 -0400

    benchmark changed

--- RandomCaseFiles/ShowFigure.py
+++ RandomCaseFiles/ShowFigure.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_agraph import read_dot
 
-path = f"Section_2/Node2_17_14_3_10_4_2_1_0/Edge8|15_14_1_flow.dot"
+path = f"Section_1/Node1_10_7_3_5_2_1_2_0/Edge1|16_14_2_flow.dot"
 G = read_dot(path)
 pos = nx.spring_layout(G)
 color_list = ['blue' for i in range(len(G.nodes()))]
